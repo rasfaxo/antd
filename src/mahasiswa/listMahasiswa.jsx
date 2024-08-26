eebb3f702d0a5d6b37d2c5d0748e0c1eb802196f
Naufal Farraz Al <naufalfarraz30098@gmail.com>
2024-08-27 00:19:17 +0700

    refactor: Update ListMahasiswa component for better data display and user interaction

--- src/mahasiswa/listMahasiswa.jsx
+++ src/mahasiswa/listMahasiswa.jsx
@@ -4,7 +4,7 @@ import supabase from "../connector";
 import ModalForm from "../components/ModalForm";
 
 const ListMahasiswa = () => {
-  const [openModal, setOpenModal] = useState(true);
+  const [openModal, setOpenModal] = useState(false);
   const [dataMhs, setDataMhs] = useState([]);
 
   const column = [
@@ -49,33 +49,39 @@ const ListMahasiswa = () => {
           title="Add user"
         />
       )}
-      <header className="flex items-center justify-between mb-4">
-        <div className="flex flex-col">
-        <h1 className="text-3xl font-bold">User Management</h1>
-        <p className="text-gray-500 font-semibold">Search, filter, and manage your users with ease</p>
-        </div>
-        <div className="flex items-center space-x-2">
-          <input
-            type="text"
-            placeholder="Search"
-            className="px-4 py-2 border rounded"
+      <div className="w-[95%] mx-auto">
+        <header className=" flex items-center justify-between mb-4">
+          <div className="flex flex-col">
+            <h1 className="text-3xl font-bold">User Management</h1>
+            <p className="text-gray-500 font-semibold">
+              Search, filter, and manage your users with ease
+            </p>
+          </div>
+          <div className="flex items-center space-x-2">
+            <input
+              type="text"
+              placeholder="Search"
+              className="px-4 py-2 border rounded"
+            />
+            <button className="px-4 py-2 bg-gray-200 rounded">Filters</button>
+            <button
+              className="px-4 py-2 bg-blue-500 text-white rounded"
+              onClick={() => setOpenModal(true)}
+            >
+              Add user  
+            </button>
+          </div>
+        </header>
+        <div className="overflow-hidden">
+          <Table
+            rowKey="id"
+            columns={column}
+            rowSelection={true}
+            dataSource={dataMhs}
+            pagination={{ pageSize: 9, position: ['bottomCenter'] }} 
+            
           />
-          <button className="px-4 py-2 bg-gray-200 rounded">Filters</button>
-          <button
-            className="px-4 py-2 bg-blue-500 text-white rounded"
-            onClick={() => setOpenModal(true)}
-          >
-            Add user
-          </button>
         </div>
-      </header>
-      <div className="overflow-hidden w-[90%] mx-auto">
-        <Table
-          rowKey="id"
-          columns={column}
-          rowSelection={true}
-          dataSource={dataMhs}
-        />
       </div>
     </main>
   );
