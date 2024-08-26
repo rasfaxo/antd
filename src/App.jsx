ed711c796fa1fa14c3f9f0953ec48f247fefd169
Naufal Farraz Al <naufalfarraz30098@gmail.com>
2024-08-26 21:40:18 +0700

    refactor: Update App component for improved routing and state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Login from "./components/Login";
 import { Routes, Route } from "react-router-dom";
 import Register from "./components/Register";
 import Layout from "./pages/Layout";
+import supabase from "./connector";
+import ListMahasiswa from "./mahasiswa/listMahasiswa";
 
 const App = () => {
   const [session, setSession] = useState(false);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+
+    return () => subscription.unsubscribe()
+  }, [])
 
   if (!session) {
     return (
@@ -20,7 +36,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<h1>Dashboard</h1>} />
-        <Route path="/mahasiswa" element={<h1>Mahasiswa</h1>} />
+        <Route path="/mahasiswa" element={<ListMahasiswa />} />
         <Route path="/settings" element={<h1>Settings</h1>} />
       </Route>
     </Routes>
