d720c26bcf16b22968dff420f8f01d55e48d42c8
Naufal Farraz Al <naufalfarraz30098@gmail.com>
2024-09-02 23:16:01 +0700

    feat: add student list management with search, filter, and CRUD operations using Ant Design and Supabase

--- src/mahasiswa/listMahasiswa.jsx
+++ src/mahasiswa/listMahasiswa.jsx
@@ -1,20 +1,77 @@
-import { Table } from "antd";
+import { Modal, Table, Popconfirm, Button, message, FloatButton, Avatar, Input } from "antd";
 import React, { useEffect, useState } from "react";
 import supabase from "../connector";
 import ModalForm from "../components/ModalForm";
+import Chance from "chance";
+import { IoIosAdd } from "react-icons/io";
+import { IoFilterSharp } from "react-icons/io5";
+import ModalFormEdit from "../components/ModalFormEdit";
 
 const ListMahasiswa = () => {
   const [openModal, setOpenModal] = useState(false);
+  const [openModalEdit, setOpenModalEdit] = useState(false);
+  const [dataEdit, setDataEdit] = useState({});
   const [dataMhs, setDataMhs] = useState([]);
+  const [refresh, setRefresh] = useState(false);
+  const [selectedRowKeys, setSelectedRowKeys] = useState([]);
 
+  const chance = new Chance();
+  const { confirm } = Modal;
+  const { Search} = Input;
+  const { success } = message;
+
+  //handle generate fake data
+  function genFakeData() {
+    confirm({
+      title: "apakah anda ingin menambah 20 fake data?",
+      onOk() {
+        let fakeData = [];
+
+        for (let i = 0; i <= 20; i++) {
+          let name = chance.name();
+          let nim = chance.integer();
+          let alamat = chance.address();
+          let telp = chance.phone();
+
+          fakeData.push({
+            name: name,
+            nim: nim,
+            alamat: alamat,
+            telp: telp,
+          });
+        }
+
+        supabase
+          .from("mahasiswa")
+          .insert(fakeData)
+          .then((res) => {
+            setRefresh((prev) => (prev = !prev));
+            success("Berhasil menambahkan 20 data");
+          });
+      },
+    });
+  }
+  //handle refresh
+  function handleRefresh() {
+    setRefresh((prev) => !prev);
+  }
+  // column table
   const column = [
-    {
-      title: "Id",
-      dataIndex: "id",
-    },
+    // {
+    //   title: "No",
+    //   dataIndex: "id",
+    // },
     {
       title: "Nama Mahasiswa",
       dataIndex: "name",
+      render: (text) => {
+        return (
+          <div className="flex items-center gap-3">
+            <Avatar src={`https://picsum.photos/200/300?random=${chance.integer()}`} />
+            <p>{text}</p>
+          </div>
+        );
+      },
     },
     {
       title: "NIM",
@@ -28,58 +85,165 @@ const ListMahasiswa = () => {
       title: "Telepon",
       dataIndex: "telp",
     },
+    {
+      title: "Action",
+      render: (content, data) => (
+        <div className="flex gap-3">
+          <div>
+            <Popconfirm
+              title="Apakah anda yakin ingin delete data ini ?"
+              okText="Delete"
+              onConfirm={() => handleSingleDelete(data.id)}
+              cancelText="Tidak "
+            >
+              <Button type="primary" danger>
+                Delete
+              </Button>
+            </Popconfirm>
+          </div>
+          <Button
+            type="primary"
+            onClick={() => {
+              setDataEdit(data);
+              setOpenModalEdit(true);
+            }}
+          >
+            Update
+          </Button>
+        </div>
+      ),
+    },
   ];
+  // row selection
+  const rowSelection = {
+    selectedRowKeys,
+    onChange: onSelectChange,
+  };
 
+  //handle single delete
+  function handleSingleDelete(id) {
+    supabase
+      .from("mahasiswa")
+      .delete()
+      .eq("id", id)
+      .then((res) => {
+        setRefresh((prev) => (prev = !prev));
+      });
+  }
+
+  //handle multiple delete
+  function handleMultipleDelete() {
+    confirm({
+      title: "Apakah anda ingin menghapus data ini?",
+      onOk() {
+        supabase
+          .from("mahasiswa")
+          .delete()
+          .in("id", selectedRowKeys)
+          .then((res) => {
+            setRefresh((prev) => (prev = !prev));
+            setSelectedRowKeys([]);
+            success("Berhasil menghapus data");
+          });
+      },
+    });
+  }
+
+  //handle row selection
+  function onSelectChange(newSelectedRowKeys) {
+    setSelectedRowKeys(newSelectedRowKeys);
+  }
+
+  function handleSearch(e) {
+    supabase
+      .from("mahasiswa")
+      .select("*")
+      .ilike("name", `%${e}%`)
+      .order("id", { ascending: false })
+      .then((res) => {
+        setDataMhs(res.data);
+      });
+  }
+
+  //fetch data
   useEffect(() => {
     supabase
       .from("mahasiswa")
       .select("*")
+      .order("id", { ascending: false })
       .then((res) => {
         setDataMhs(res.data);
       });
-  }, []);
+  }, [refresh]);
 
   return (
-    <main>
+    <main className="font-sans">
       {openModal && (
         <ModalForm
           open={openModal}
           onCancel={() => setOpenModal(false)}
-          onOk={() => setOpenModal(false)}
-          title="Add user"
+          onRefresh={() => setRefresh(!refresh)}
+          isRefresh={handleRefresh}
+        />
+      )}
+      {openModalEdit && (
+        <ModalFormEdit
+          isOpen={openModalEdit}
+          onCancel={() => setOpenModalEdit(false)}
+          onRefresh={() => setRefresh(!refresh)}
+          isRefresh={handleRefresh}
+          data={dataEdit}
         />
       )}
       <div className="w-[95%] mx-auto">
         <header className=" flex items-center justify-between mb-4">
           <div className="flex flex-col">
             <h1 className="text-3xl font-bold">User Management</h1>
-            <p className="text-gray-500 font-semibold">
+            <p className="text-gray-500 font-thin">
               Search, filter, and manage your users with ease
             </p>
           </div>
           <div className="flex items-center space-x-2">
-            <input
-              type="text"
-              placeholder="Search"
-              className="px-4 py-2 border rounded"
-            />
-            <button className="px-4 py-2 bg-gray-200 rounded">Filters</button>
+              <Search
+                allowClear
+                enterButton
+                onSearch={handleSearch}
+                placeholder="Search"
+                className="w-[200px]  rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500"
+              />
+            <button className="px-4 py-[5px] border border-gray-300 rounded-md flex items-center gap-2 text-gray-500">
+              <IoFilterSharp className="text-gray-500" /> Filters
+            </button>
             <button
-              className="px-4 py-2 bg-blue-500 text-white rounded"
+              className="px-2 py-[5px] bg-black r text-white rounded-md flex items-center"
               onClick={() => setOpenModal(true)}
             >
-              Add user  
+              <IoIosAdd size={21} /> Add user
             </button>
+            <FloatButton
+              type="primary"
+              icon={<IoIosAdd />}
+              onClick={genFakeData}
+            />
+            {selectedRowKeys.length > 0 && (
+              <button
+                className="px-4 py-[5px] bg-red-500 text-white rounded"
+                type="primary"
+                danger
+                onClick={handleMultipleDelete}
+              >
+                Hapus {selectedRowKeys.length} data
+              </button>
+            )}
           </div>
         </header>
         <div className="overflow-hidden">
           <Table
             rowKey="id"
             columns={column}
-            rowSelection={true}
+            rowSelection={rowSelection}
             dataSource={dataMhs}
-            pagination={{ pageSize: 9, position: ['bottomCenter'] }} 
-            
+            pagination={{ pageSize: 6, position: ["bottomCenter"] }}
           />
         </div>
       </div>
